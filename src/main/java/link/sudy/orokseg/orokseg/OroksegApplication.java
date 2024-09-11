df23a08fec6865a3e3c477196debf828d2798702
Barnabas Sudy <barnabas.sudy@gmail.com>
2024-09-11 08:29:30 +0200

    Inital project structure

---
+++ src/main/java/link/sudy/orokseg/orokseg/OroksegApplication.java
@@ -0,0 +1,13 @@
+package link.sudy.orokseg.orokseg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OroksegApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OroksegApplication.class, args);
+	}
+
+}
---
+++ src/test/java/link/sudy/orokseg/orokseg/OroksegApplicationTests.java
@@ -0,0 +1,13 @@
+package link.sudy.orokseg.orokseg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OroksegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
