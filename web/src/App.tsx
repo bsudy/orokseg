e2732b27910504085997da2e5bb8c6d15d9c33fc
Barnabas Sudy <barnabas.sudy@gmail.com>
2024-09-19 14:18:00 +0200

    Added Home page and React Router
    Add React Router in order to be able to manage the navigation routes.
    I also added a Home.tsx as the default landing page. It might
    change in the future once authentication is added to the system.
    
    The Persons.tsx page is not behind the usr /people and the main
    nav bar has a button navigating there.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,88 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
 import { Persons } from "./pages/Persons";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  CssBaseline,
+} from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  header: {},
+  title: {
+    flexGrow: 1,
+  },
+  main: {},
+  footer: {
+    padding: 4,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/people",
+      element: <Persons />,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <Persons />
+    <div className={classes.root}>
+      <header className={classes.header}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Örökség
+            </Typography>
+            <Button
+              color="inherit"
+              onClick={(evt) => {
+                evt.preventDefault();
+                router.navigate("/");
+              }}
+            >
+              Home
+            </Button>
+            <Button
+              color="inherit"
+              onClick={(evt) => {
+                evt.preventDefault();
+                router.navigate("/people");
+              }}
+            >
+              People
+            </Button>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
       </header>
+      <main>
+        <RouterProvider router={router} />
+      </main>
+      {/* <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+      </footer> */}
     </div>
   );
 }
